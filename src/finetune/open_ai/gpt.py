30c89d575b7e18f4f866741087ac6625cfa61ba7
gimhansandeeptha <rpgimhansandeeptha@gmail.com>
2024-04-04 12:07:56 +0530

    minor changes

--- src/finetune/open_ai/gpt.py
+++ src/finetune/open_ai/gpt.py
@@ -4,7 +4,7 @@ import asyncio
 
 class GPT:
     def __init__(self) -> None:
-        self.client = OpenAI(api_key="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")
+        self.client = OpenAI()
         self.instruction = "You will be provided with a text, and your task is to classify its sentiment as positive, neutral, or negative."
         self.model = "gpt-3.5-turbo"
         self.instruction_role = "system"
